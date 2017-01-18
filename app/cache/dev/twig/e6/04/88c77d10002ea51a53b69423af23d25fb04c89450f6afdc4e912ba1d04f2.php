0fbc942c8e35d3c90abdd452db6eb265f3ce0abf
JoshuaAlmeida <joshua93x@gmail.com>
2017-01-18 00:16:53 +0000

    corregido pequeño error en la vista de competicion

--- app/cache/dev/twig/e6/04/88c77d10002ea51a53b69423af23d25fb04c89450f6afdc4e912ba1d04f2.php
+++ app/cache/dev/twig/e6/04/88c77d10002ea51a53b69423af23d25fb04c89450f6afdc4e912ba1d04f2.php
@@ -148,15 +148,43 @@ class __TwigTemplate_e60488c77d10002ea51a53b69423af23d25fb04c89450f6afdc4e912ba1
         // line 24
         $context['_parent'] = (array) $context;
         $context['_seq'] = twig_ensure_traversable((isset($context["competiciones"]) ? $context["competiciones"] : $this->getContext($context, "competiciones")));
+        $context['loop'] = array(
+          'parent' => $context['_parent'],
+          'index0' => 0,
+          'index'  => 1,
+          'first'  => true,
+        );
+        if (is_array($context['_seq']) || (is_object($context['_seq']) && $context['_seq'] instanceof Countable)) {
+            $length = count($context['_seq']);
+            $context['loop']['revindex0'] = $length - 1;
+            $context['loop']['revindex'] = $length;
+            $context['loop']['length'] = $length;
+            $context['loop']['last'] = 1 === $length;
+        }
         foreach ($context['_seq'] as $context["_key"] => $context["competicion"]) {
             // line 25
             echo "                    ";
             if (($this->getAttribute($context["competicion"], "estadoCompeticion", array()) == "Finalizado")) {
-                // line 27
-                echo "                    ";
+                // line 26
+                echo "                        <a href=\"";
+                echo twig_escape_filter($this->env, $this->env->getExtension('routing')->getUrl("moduloclientes_cliente_competicionClientes", array("id" => $this->getAttribute($context["competicion"], "id", array()))), "html", null, true);
+                echo "\" class=\"fila ";
+                echo twig_escape_filter($this->env, twig_cycle(array(0 => "par", 1 => "impar"), $this->getAttribute($context["loop"], "index", array())), "html", null, true);
+                echo "\">";
+                echo twig_escape_filter($this->env, $this->getAttribute($context["competicion"], "nombre", array()), "html", null, true);
+                echo "</a>
+                    ";
             }
             // line 28
             echo "                ";
+            ++$context['loop']['index0'];
+            ++$context['loop']['index'];
+            $context['loop']['first'] = false;
+            if (isset($context['loop']['length'])) {
+                --$context['loop']['revindex0'];
+                --$context['loop']['revindex'];
+                $context['loop']['last'] = 0 === $context['loop']['revindex0'];
+            }
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['competicion'], $context['_parent'], $context['loop']);
@@ -180,6 +208,6 @@ class __TwigTemplate_e60488c77d10002ea51a53b69423af23d25fb04c89450f6afdc4e912ba1
 
     public function getDebugInfo()
     {
-        return array (  165 => 29,  159 => 28,  156 => 27,  153 => 25,  149 => 24,  146 => 23,  132 => 22,  122 => 20,  119 => 19,  102 => 18,  99 => 17,  85 => 16,  75 => 14,  72 => 13,  55 => 12,  46 => 5,  43 => 4,  37 => 3,  11 => 1,);
+        return array (  193 => 29,  179 => 28,  169 => 26,  166 => 25,  149 => 24,  146 => 23,  132 => 22,  122 => 20,  119 => 19,  102 => 18,  99 => 17,  85 => 16,  75 => 14,  72 => 13,  55 => 12,  46 => 5,  43 => 4,  37 => 3,  11 => 1,);
     }
 }
